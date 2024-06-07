a50c80c866493838b5965619318f0e34fcac0b1c
Jesse Wierzbinski <contact@cpluspatch.com>
2024-06-07 12:00:38 -1000

    feat(client): :label: Export Output and Token types

--- client/index.ts
+++ client/index.ts
@@ -1,4 +1,5 @@
-import { ResponseError } from "./lysand/base";
+import { type Output, ResponseError } from "./lysand/base";
 import { LysandClient } from "./lysand/lysand";
+import type { Token } from "./types/token";
 
-export { LysandClient, ResponseError };
+export { LysandClient, ResponseError, type Output, type Token };
